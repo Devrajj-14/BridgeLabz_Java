77be6f52fb8729b32f2aa8da994751d0ca3955b0
Devrajj-14 <dg9770@srmist.edu.in>
2026-02-25 12:30:40 +0530

    [Devraj]:fix valid email code

--- BL/src/main/java/com/bl/ValidEmail.java
+++ BL/src/main/java/com/bl/ValidEmail.java
@@ -3,10 +3,11 @@ package com.bl;
 import java.util.regex.Pattern;
 
 public class ValidEmail {
-    // local: abc OR abc.xyz
-    // domain: bl.co OR bl.co.in
-    private static final Pattern PATTERN =
-            Pattern.compile("^[a-z]+(\\.[a-z]+)?@bl\\.co(\\.in)?$");
+
+    private static final Pattern PATTERN = Pattern.compile(
+            "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@" +
+                    "[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$"
+    );
 
     public boolean validate(String email) {
         return email != null && PATTERN.matcher(email).matches();
